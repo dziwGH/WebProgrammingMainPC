8b94b39c9f00f74e414da1a7604b470c2171c3f3
dziwGH <dziwanowskia@atu.edu>
2025-02-17 22:11:46 -0600

    added a company list

--- Day 10/script.js
+++ Day 10/script.js
@@ -13,3 +13,20 @@ document.getElementById("fetchUsers").addEventListener("click", function () {
     })
     .catch(error => console.error("Error fetching data", error));
 });
+
+document.getElementById("fetchCompany").addEventListener("click", function () {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(data=>{
+        let companyList = document.getElementById("companyList");
+        companyList.innerHTML="";
+
+        data.forEach(user => {
+            let li = document.createElement("li");
+            li.innerHTML = `<strong> ${user.company.name}</strong>`;
+            companyList.appendChild(li);
+        });
+    })
+    .catch(error => console.error("Error fetching data", error));
+});
+
